6b641de1c2684d2ec05fe740ccd504db22c637d4
Matthias Gattermeier <matthias.gattermeier@gmail.com>
2015-08-06 12:08:34 -0700

    feat/sockets add basic socket event handling and fakedata for testing

---
+++ sockets/fakedata.js
@@ -0,0 +1,35 @@
+exports.data = [{
+      data: 'test',
+      comments: [{
+        person: {
+          displayname: 'Name 1'
+        },
+        video: {},
+        text: 'Lipsum 1',
+        votes: 3,
+        timestamp: ''
+      }, {
+        person: {
+          displayname: 'Name 2'
+        },
+        video: {},
+        text: 'Lipsum 2',
+        votes: 0,
+        timestamp: ''
+      }, {
+        person: {
+          displayname: 'Name 3'
+        },
+        video: {},
+        text: 'Lipsum 3',
+        votes: 18,
+        timestamp: ''
+      }, {
+        person: {
+          displayname: 'Name 4'
+        },
+        video: {},
+        text: 'Lipsum 4',
+        votes: -3,
+        timestamp: ''
+      }];
--- sockets/index.js
+++ sockets/index.js
@@ -1,3 +1,36 @@
+var fakedata = require('./fakedata').data;
 module.exports = function(io) {
+  // io.connection
+  // listen to socket event 'cs-init':
+  // // contains: video id, user objectID or empty
+  // // video ID = room
+  // // remove socekt from all other rooms 
+  // // add socket to room, 
+  // socket.emit 'sc-init' with fake video info ..
+  // // contains: sample comments [{text : string, username, .. timestamp }]
+  // 
+  // listening to socket event 'cs-comment'
+  // // contain: video id, user objectID, text, timestamp ... 
 
+  io.connection(function(socket) {
+    // listen to init event from client
+    socket.on('cs-init', function(data) {
+      console.log(data);
+      // data contains video id info which represents the namespace the socket needs to join
+      var channel = data.videoid;
+      // socket should leave all other namespaces / rooms
+      if (socket.lastChannel) {
+        socket.leave(socket.lastChannel);
+        socket.lastChannel = null;
+      }
+      // socket needs to join the namespace of the video id
+      socket.join(channel);
+      socket.lastChannel = channel;
+      // we emit a server-client event to the socket 
+      socket.emit('sc-init', fakedata)
+    });
+    socket.on('disconnect', function() {
+      io.emit('user disconnected');
+    });
+  })
 }
