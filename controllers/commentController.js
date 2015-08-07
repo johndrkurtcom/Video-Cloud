657c4bef9595c910fb86eebf8494a3f7a7435093
Ian Conery <ianconery@gmail.com>
2015-08-06 17:29:03 -0700

    added user video and comment controller files

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,6 +1,19 @@
 var mongoose = require('mongoose');
 var Comment = require('../models/comment.js');
 
-module.exports(function(err, comment){
-  
+
+
+console.log('inside the comment controller')
+var newComment = new Comment({
+  text: 'Hello World'
+});
+
+newComment.save(function(err, message){
+  console.log('inside the save method')
+  if(err){
+    console.log(err);
+  }else{
+    console.log('This is the fake message' + message);
+  }
+
 });
