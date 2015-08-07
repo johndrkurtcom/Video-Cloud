f73c6b4256c0add7d7faf48cc34fbd74e3df9f26
Matthias Gattermeier <matthias.gattermeier@gmail.com>
2015-08-06 20:07:55 -0700

    feat/sockets - broadcast new comments to corresponding namespace

--- sockets/index.js
+++ sockets/index.js
@@ -1,42 +1,26 @@
-var fakedata = require('./fakedata').data;
 var Video = require('../models').Video;
-var addComment = require('../controllers/commentController.js').addComment;
+var addComment = require('../controllers/commentController.js').add;
+
+var setVideoChannel = function(socket, data) {
+  // data contains video id info which represents the namespace the socket needs to join
+  var channel = data.videoId;
+  // socket should leave all other namespaces / rooms
+  if (socket.lastChannel) {
+    socket.leave(socket.lastChannel);
+    socket.lastChannel = null;
+  }
+  // socket needs to join the namespace of the video id
+  socket.join(channel);
+  socket.lastChannel = channel;
+}
+
 
 module.exports = function(io) {
-  // console.log(fakedata);
-  // io.connection
-  // listen to socket event 'cs-init':
-  // // contains: video id, user objectID or empty
-  // // video ID = room
-  // // remove socekt from all other rooms 
-  // // add socket to room, 
-  // socket.emit 'sc-init' with fake video info ..
-  // // contains: sample comments [{text : string, username, .. timestamp }]
-  // 
-  // listening to socket event 'cs-comment'
-  // // contain: video id, user objectID, text, timestamp ... 
-
-  var setVideoChannel = function(socket, data) {
-    // data contains video id info which represents the namespace the socket needs to join
-    var channel = data.videoId;
-    // socket should leave all other namespaces / rooms
-    if (socket.lastChannel) {
-      socket.leave(socket.lastChannel);
-      socket.lastChannel = null;
-    }
-    // socket needs to join the namespace of the video id
-    socket.join(channel);
-    socket.lastChannel = channel;
-  }
 
   io.on('connection', function(socket) {
-    console.log('connected');
-
     // listen to init event from client
     socket.on('cs-init', function(data) {
-      console.log(data);
       setVideoChannel(socket, data);
-
       Video.findOne({
         videoId: data.videoId
       }, function(err, video) {
@@ -45,26 +29,26 @@ module.exports = function(io) {
         socket.emit('sc-init', {
           video: video
         })
-
       })
-
-
     });
 
     // listen to new comments from socket
-    socket.on('cs-comment', function(data) {
+    socket.on('cs-comment', function(comment) {
       // add comment to video 
-      addComment(data, function(err, data) {
+      addComment(comment, function(err, video) {
         if (err) {
           // if something went wrong, communicate the error to client
           socket.emit('sc-comment error', {
             error: err
           });
         } else {
-          // if comment successfully added to video, return entire video object to client
+          // if comment successfully added to video
+          // return entire video object to socket -- maybe redundant..
           socket.emit('sc-comment success', {
-            success: data
+            success: video
           });
+          // send comment to all (!) connected clients in channel
+          io.to(video.videoId).emit('sc-comment new', comment);
         }
       })
     });
